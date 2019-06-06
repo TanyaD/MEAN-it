3ab39af37ee9ed5bf02ef9dda470f3ea1431e107
Tanya Dubinchyk <tat.dbk@gmail.com>
2019-06-06 16:35:37 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function(request, response){
+    console.log('client request URL:', request.url);
+    //routing
+    if(request.url === '/'){
+        fs.readFile('index.html','utf8', function(errors,contents){
+            response.writeHead(200,{'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end()
+        });
+    }
+    else{
+        response.writeHead(404);
+        response.end('File not found!');
+    }
+})
+//server port
+server.listen(6789);
+console.log('Running in localhost at port 6789');
